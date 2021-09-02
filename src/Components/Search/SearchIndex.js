7a055aaaba5fed3cb9783df6ff62c03725af0163
Brad Titus <bltitus@me.com>
2021-09-02 00:37:00 -0400

    work on Search module, still some problems with how it updates

--- src/Components/Search/SearchIndex.js
+++ src/Components/Search/SearchIndex.js
@@ -1,21 +1,66 @@
 import React from 'react';
 import {Input} from 'reactstrap';
  
-const SearchIndex extends Component() {
-   this.state = {
-     things: ['pen', 'marker', 'eraser', 'notebook', 'pencil', 'scissors', 'highlighter', 'stapler', 'paper clip', 'binder', 'hole punch', 'laminator', 'laminating sheets', 'protective sheets', 'index cards']
-   }
- }
+class SearchIndex extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+    things: ['pen', 'marker', 'eraser', 'notebook', 'pencil', 'scissors', 'highlighter', 'stapler', 'paper clip', 'binder', 'hole punch', 'laminator', 'laminating sheets', 'protective sheets', 'index cards'],
+    filtered: []
+  }
+  }
 
- function searchFunction() {
- }
+  componentDidMount() {
+    this.setState({
+      filtered: this.state.things
+    });
+    console.log(this.state.things);
+  }
 
- render() {
-     <div>
-       <Input placeholder='Search Here' />
-       <h3>Results:</h3>
-     </div>
- }
+  componentDidUpdate() {
+    if(this.state.filtered !== this.state.things) {
+    this.setState({
+      filtered: this.state.things
+    });
+    }
+  }
 
- 
-export SearchIndex;
+  handleChange(e) {
+    let results = [];
+    let newResults = [];
+    if (e.target.value !== '') {
+      results = this.state.things;
+      newResults = results.filter(terms => {
+        const lower = terms.toLowerCase();
+        const filter = e.target.value.toLowerCase();
+        return lower.includes(filter);
+      });
+    } else {
+      newResults = this.state.things;
+    }
+    this.setState({
+      filtered: newResults
+    });
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  render() {
+    return(
+      <div>
+        <form>
+            <Input 
+            type="text"
+            className="input"
+            onChange={this.handleChange}
+            placeholder="search"
+             />
+        </form>
+        <h3>Results:</h3>
+          {this.state.filtered.map((items) => (
+            <li key={items}>{items}</li>
+          ))}
+      </div>
+  )}
+}
+
+export default SearchIndex;
